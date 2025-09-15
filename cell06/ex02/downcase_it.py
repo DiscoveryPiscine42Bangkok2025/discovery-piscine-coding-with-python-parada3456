e2ecbaecad02e83caeb894e7a7508d085b1580f3
parada <parada3223@gmail.com>
2025-09-15 23:45:37 +0700

    6.2

---
+++ cell06/ex02/downcase_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+def downcase_it(str1):
+    return str1.lower()
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    for i in sys.argv[1:]:
+        print(downcase_it(i))
