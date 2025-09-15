9b2596f1b5bd0457fc1e8cd04f105694ce00c9bd
parada <parada3223@gmail.com>
2025-09-15 23:26:09 +0700

    5.09 new ways

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import re
 
 if len(sys.argv) != 3:
     print("none")
 else:
     count = sys.argv[2].count(sys.argv[1])
+    # matches = re.findall(sys.argv[1],sys.argv[2])
+    # count = len(matches)
     if count == 0:
         print("none")
     else :
