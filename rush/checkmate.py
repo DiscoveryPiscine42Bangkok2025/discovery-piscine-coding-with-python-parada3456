f98d6dc045a0bdcbf3906bba2ed07f5a9113eec9
parada <parada3223@gmail.com>
2025-09-18 06:07:13 +0700

    fix bug Q, P

--- rush/checkmate.py
+++ rush/checkmate.py
@@ -8,27 +8,28 @@ def checkmate(board_raw):
             
             straight_dir = [(1,0), (-1,0), (0,1), (0,-1)]
             diag_dir = [(1,1), (1,-1), (-1,1), (-1,-1)]
-            pawns_dir = [(-1,-1),(1,-1)]
+            queen_dirs = [(1,1), (1,-1), (-1,1), (-1,-1), (1,0), (-1,0), (0,1), (0,-1)]
+            pawns_dir = [(-1,-1),(-1,1)]
             
             for i, row in enumerate(board):
                 for j, col in enumerate(row):
                     if col == "P":
-                        if isFoundKingFirst((i, j), pawns_dir, board, col):
+                        if isFoundKingFirst((i, j), pawns_dir, board, "P"):
                             display(board)
                             print("Fail by P\n")
                             return
                     elif col == "B":
-                        if isFoundKingFirst((i, j), diag_dir, board, col):
+                        if isFoundKingFirst((i, j), diag_dir, board):
                             display(board)
                             print("Fail by B\n")
                             return
                     elif col == "R":
-                        if isFoundKingFirst((i, j), straight_dir, board, col):
+                        if isFoundKingFirst((i, j), straight_dir, board):
                             display(board)
                             print("Fail by R\n")
                             return
                     elif col == "Q":
-                        if isFoundKingFirst((i, j), straight_dir + diag_dir, board, col):
+                        if isFoundKingFirst((i, j), straight_dir + diag_dir, board, "Q"):
                             display(board)
                             print("Fail by Q\n")
                             return
@@ -71,26 +72,42 @@ def isBoardValid(board):
 #                 return (i, j)
 #     return None
     
-def isFoundKingFirst(piece_pos, piece_dirs, board, piece):
+def isFoundKingFirst(piece_pos, piece_dirs, board, piece = ""):
     if piece_pos is None:
         return False
     
     start_r, start_c = piece_pos
+
     size = len(board)
+
+    dict_found = {"P":9, "B":9, "R":9, "Q":9,"K":9}
 
     for dr, dc in piece_dirs:
         r, c = start_r + dr, start_c + dc
+        distance = 0
         while 0 <= r < size and 0 <= c < size:
-            cell = board[r][c]
-            if cell in '.*':
+            cell = board[r][c] 
+            if cell in ".*":
                 board[r][c] = "*"
+                    
             elif cell == 'K':
-                    return True   
+                dict_found["K"] = distance
+
             else:
-                return False 
+                if distance < dict_found[cell]:
+                    dict_found[cell] = distance
+            
+            if piece == "P":
+                break
+
             r += dr
             c += dc
-    return False 
+            distance += 1
+
+    if min(dict_found, key=dict_found.get) == "K" and min(dict_found.values()) != 9:
+        return True
+    else:
+        return False 
 
 def display(board):
     for row in board:
